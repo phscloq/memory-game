117067abab34edee3215bbd651cd3367ffa3d49a
Baran Dogan <phscloq@gmail.com>
2023-07-08 01:29:01 +0300

    ADD shuffle function

---
+++ src/app/utils/shuffle.tsx
@@ -0,0 +1,16 @@
+export default function Shuffle({pokemonList}:{pokemonList:any}){
+    const temporaryArr = [];
+
+while(pokemonList.length>=1){
+    let index = Math.floor(Math.random()*12);
+    if(pokemonList[index]!=undefined){
+        temporaryArr.push(pokemonList[index])
+        pokemonList.splice(index, 1);
+    }
+    
+}
+for(let i=0; i<12; i++){
+    pokemonList.push(temporaryArr[i]);
+}
+return pokemonList;
+}
