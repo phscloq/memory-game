b90e97c4d196a7be713d135186d2cfbd783bcc88
Baran Dogan <phscloq@gmail.com>
2023-07-08 14:32:03 +0300

    ADD game component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,20 @@
 
-import Image from 'next/image'
-import Pokemon from './components/pokemon'
 import getPokemon from './lib/pokemon-data'
-import Score from './components/score';
-import Shuffle from './utils/shuffle';
-import Game from './components/game';
-export default async function Home() {
-  const pokemonList = await getPokemon();
 
-  Shuffle({pokemonList});
+import Game from './components/game';
+
+
+export default async function Home() {
  
+
+  const pokemonList = await getPokemon(); 
   return (
 
     <main className="flex min-h-screen flex-col items-center justify-center px-60">
-      <h1 className='text-6xl text-center mb-16'>Catch Your Pokemon</h1>
-      <Score />
-<Pokemon 
+      <h1 className='text-6xl text-center mb-16 sm:mb-2'>Catch Your Pokemon</h1>
+    <Game 
     pokemonList={pokemonList}
-
-/>
+    />
     </main>
   )
 }
